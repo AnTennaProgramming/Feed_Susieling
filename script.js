8adad562f4284bc7768a339d5a481f58ccecc572
AnTennaProgramming <tiltadodof0rt6519@gmail.com>
2026-01-20 17:07:34 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-s
+function growth() { let img = document.getElementById("Susieling");
+  let currentHeight = img.clientHeight;
+            let currentWidth = img.clientWidth;
+  img.style.width= (currentWidth + 1) + "px";
+img.style.height= (currentHeight + 1) + "px";}
